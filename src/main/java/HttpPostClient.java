b2f093256367a329f9fdd5055418f4bb72ac4932
Roman Voievidko <roman.voievidko@synapse.com>
2017-12-21 12:47:19 +0200

    Working code

--- src/main/java/HttpPostClient.java
+++ src/main/java/HttpPostClient.java
@@ -1,3 +1,8 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.From;
+import entity.Train;
+import mappers.TrainSerializer;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
@@ -6,51 +11,70 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class HttpPostClient {
     private final String URL = "https://booking.uz.gov.ua/purchase/search/";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args){
+        HttpPostClient httpPostClient = new HttpPostClient();
+        boolean flag = true;
+        while (flag){
+            try {
+                int counter = httpPostClient.printNumberOfTrains();
+                if (counter>2){
+                    System.out.println("BUY!!!!!!!");
+                    JOptionPane.showMessageDialog(null, "Buy", "InfoBox: ", JOptionPane.INFORMATION_MESSAGE);
+                    flag = false;
+                } else {
+                    Random rnd = new Random();
+                    int sleepTime = (int)(5000 + 10000 * rnd.nextDouble());
+                    Thread.sleep(sleepTime);
+                    System.out.println("Number of trains now: " + counter + "  Sleep time: " + sleepTime);
+
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+    }
+
+    private int printNumberOfTrains() throws Exception {
         HttpPostClient http = new HttpPostClient();
-        http.sendPost();
+        String JsonLine = http.sendPost();
+//        System.out.println(JsonLine);
+
+        int index = JsonLine.indexOf("num");
+        int count = 0;
+        while (index != -1) {
+            count++;
+            JsonLine = JsonLine.substring(index + 1);
+            index = JsonLine.indexOf("num");
+        }
+
+//        System.out.println(count);
+        return count;
     }
 
     // HTTP POST request
-    private void sendPost() throws Exception {
+    private String sendPost() throws Exception {
         HttpClient client = HttpClients.createDefault();
-        //HttpClient client = new DefaultHttpClient();
         HttpPost post = new HttpPost(URL);
 
-        // add header
-
-//        post.setHeader("Accept", "*/*");
-//        post.setHeader("Content-Type","application/x-www-form-urlencoded");
-//        post.setHeader("GV-Ajax", "1");
-//        post.setHeader("GV-Screen", "1920x1080");
-//        post.setHeader("Referer", "https://booking.uz.gov.ua/");
-//        post.setHeader("Accept-Language", "ru-RU");
-//        post.setHeader("Accept-Encoding", "gzip, deflate");
-//        post.setHeader("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko");
-//        post.setHeader("Host", "booking.uz.gov.ua");
-//        post.setHeader("Content-Length", "263");
-//        post.setHeader("DNT", "1");
-//        post.setHeader("Connection", "Keep-Alive");
-//        post.setHeader("Cache-Control", "no-cache");
-//        post.setHeader("Cookie", "_gv_lang=uk; __utma=31515437.1167593834.1426147746.1447325206." +
-//                "1450785433.5; _gv_sessid=dtfvv9vh1smpdups284hj2isb0; HTTPSERVERID=server1; _ga=GA1.3.1167593" +
-//                "834.1426147746; _gid=GA1.3.603776797.1508740788; _gat=1");
-
-
         List<NameValuePair> urlParameters = new ArrayList<NameValuePair>();
-        urlParameters.add(new BasicNameValuePair("station_id_from", "2200001"));
-        urlParameters.add(new BasicNameValuePair("station_id_till", "2218200"));
-        urlParameters.add(new BasicNameValuePair("station_from", "Київ"));
-        urlParameters.add(new BasicNameValuePair("station_till", "Івано-Франківськ"));
-        urlParameters.add(new BasicNameValuePair("date_dep", "30.11.2017"));
+        urlParameters.add(new BasicNameValuePair("station_id_from", "2218200"));
+        urlParameters.add(new BasicNameValuePair("station_id_till", "2200001"));
+        urlParameters.add(new BasicNameValuePair("station_from", "Івано-Франківськ"));
+        urlParameters.add(new BasicNameValuePair("station_till", "Київ"));
+        urlParameters.add(new BasicNameValuePair("date_dep", "22.12.2017"));
         urlParameters.add(new BasicNameValuePair("time_dep", "00:00"));
         urlParameters.add(new BasicNameValuePair("time_dep_till", ""));
         urlParameters.add(new BasicNameValuePair("another_ec", "0"));
@@ -62,9 +86,9 @@ public class HttpPostClient {
 
         HttpResponse response = client.execute(post);
 
-        System.out.println("Sending 'POST' request to URL : " + URL);
-        System.out.println("Post parameters : " + post.getEntity());
-        System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
+//        System.out.println("Sending 'POST' request to URL : " + URL);
+//        System.out.println("Post parameters : " + post.getEntity());
+//        System.out.println("Response Code : " + response.getStatusLine().getStatusCode());
 
         BufferedReader rd = new BufferedReader(
                 new InputStreamReader(response.getEntity().getContent()));
@@ -74,7 +98,7 @@ public class HttpPostClient {
         while ((line = rd.readLine()) != null) {
             result.append(line);
         }
-        System.out.println(result.toString());
+        return result.toString();
 
     }
 }
